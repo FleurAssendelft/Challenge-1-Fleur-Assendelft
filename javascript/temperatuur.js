e2c6f67895db56c445d1a765ccaede0b221bd732
Fleur Assendelft <fleurassendelft@hotmail.nl>
2018-03-05 12:15:32 +0100

    Added mars ipv moon

--- javascript/temperatuur.js
+++ javascript/temperatuur.js
@@ -40,18 +40,18 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		dataPoints: [
 			{ x: new Date(2030, 0, 3), y: -113 },
 			{ x: new Date(2030, 0, 4), y: -120 },
-			{ x: new Date(2030, 0, 5), y: -122 },
-			{ x: new Date(2030, 0, 6), y: -140 },
-			{ x: new Date(2030, 0, 7), y: -135 },
-			{ x: new Date(2030, 0, 8), y: -130 },
-			{ x: new Date(2030, 0, 9), y: -127 },
-			{ x: new Date(2030, 0, 10), y: -150 },
-			{ x: new Date(2030, 0, 11), y: -152 },
-			{ x: new Date(2030, 0, 12), y: -160 },
-			{ x: new Date(2030, 0, 13), y: -157 },
-			{ x: new Date(2030, 0, 14), y: -148 },
-			{ x: new Date(2030, 0, 15), y: -150 },
-			{ x: new Date(2030, 0, 16), y: -138 }
+			{ x: new Date(2030, 0, 5), y: -100 },
+			{ x: new Date(2030, 0, 6), y: -90 },
+			{ x: new Date(2030, 0, 7), y: -86 },
+			{ x: new Date(2030, 0, 8), y: -85 },
+			{ x: new Date(2030, 0, 9), y: -80 },
+			{ x: new Date(2030, 0, 10), y: -87 },
+			{ x: new Date(2030, 0, 11), y: -90 },
+			{ x: new Date(2030, 0, 12), y: -87 },
+			{ x: new Date(2030, 0, 13), y: -95 },
+			{ x: new Date(2030, 0, 14), y: -100 },
+			{ x: new Date(2030, 0, 15), y: -102 },
+			{ x: new Date(2030, 0, 16), y: -99 }
 		]
 	},
 	{
@@ -61,20 +61,20 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		lineDashType: "dash",
       	color: "#5c85d6",
 		dataPoints: [
-			{ x: new Date(2030, 0, 3), y: 127 },
-			{ x: new Date(2030, 0, 4), y: 120 },
-			{ x: new Date(2030, 0, 5), y: 123 },
-			{ x: new Date(2030, 0, 6), y: 100 },
-			{ x: new Date(2030, 0, 7), y: 90 },
-			{ x: new Date(2030, 0, 8), y: 97 },
-			{ x: new Date(2030, 0, 9), y: 102 },
-			{ x: new Date(2030, 0, 10), y: 125 },
-			{ x: new Date(2030, 0, 11), y: 120 },
-			{ x: new Date(2030, 0, 12), y: 122 },
-			{ x: new Date(2030, 0, 13), y: 119 },
-			{ x: new Date(2030, 0, 14), y: 109 },
-			{ x: new Date(2030, 0, 15), y: 99 },
-			{ x: new Date(2030, 0, 16), y: 104 }
+			{ x: new Date(2030, 0, 3), y: 20 },
+			{ x: new Date(2030, 0, 4), y: 18 },
+			{ x: new Date(2030, 0, 5), y: 22 },
+			{ x: new Date(2030, 0, 6), y: 25 },
+			{ x: new Date(2030, 0, 7), y: 23 },
+			{ x: new Date(2030, 0, 8), y: 19 },
+			{ x: new Date(2030, 0, 9), y: 16 },
+			{ x: new Date(2030, 0, 10), y: 18 },
+			{ x: new Date(2030, 0, 11), y: 18 },
+			{ x: new Date(2030, 0, 12), y: 20 },
+			{ x: new Date(2030, 0, 13), y: 23 },
+			{ x: new Date(2030, 0, 14), y: 21 },
+			{ x: new Date(2030, 0, 15), y: 17 },
+			{ x: new Date(2030, 0, 16), y: 15 }
 		]
 	}]
 });
